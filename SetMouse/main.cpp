287ad4ce5a618475dfad619d85615d3a0d4142dd
UnnamedOrange <lycheng1215@sina.com>
2021-01-16 22:07:41 +0800

    创建项目。

---
+++ SetMouse/main.cpp
@@ -0,0 +1,54 @@
+#include <Windows.h>
+#undef min
+#undef max
+#include <utils/window.hpp>
+
+class main_window : public window
+{
+	virtual INT_PTR WindowProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam) override
+	{
+		switch (message)
+		{
+			HANDLE_MSG(hwnd, WM_CREATE, OnCreate);
+			HANDLE_MSG(hwnd, WM_DESTROY, OnDestroy);
+
+		default:
+			return DefWindowProcW(hwnd, message, wParam, lParam);
+		}
+		return 0;
+	}
+	BOOL OnCreate(HWND hwnd, LPCREATESTRUCT lpCreateStruct)
+	{
+
+		return TRUE;
+	}
+	void OnDestroy(HWND hwnd)
+	{
+		PostQuitMessage(0);
+	}
+};
+
+int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE, _In_ LPWSTR, _In_ int)
+{
+	// 设置高 DPI 支持。
+	SetProcessDpiAwarenessContext(DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2);
+	// 单例。
+	HANDLE hMutex = CreateMutexW(nullptr, FALSE, L"Global\\SetMouse-29EC5FCE-B1F3-4F30-9A7C-73BD16382FC5");
+	DWORD last_error = GetLastError();
+	if (last_error == ERROR_ALREADY_EXISTS ||
+		last_error == ERROR_ACCESS_DENIED && !hMutex)
+	{
+		if (hMutex)
+			CloseHandle(hMutex);
+		return 0;
+	}
+	else if (!hMutex)
+		return 1;
+
+	main_window wnd;
+	wnd.create();
+	wnd.message_loop();
+
+	CloseHandle(hMutex);
+	return 0;
+}
