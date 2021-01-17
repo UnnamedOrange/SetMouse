c105529050da7ecab0c4952754a8664d92925845
UnnamedOrange <lycheng1215@sina.com>
2021-01-18 01:29:01 +0800

    补充注释；完成程序运行的基本逻辑。

--- SetMouse/main.cpp
+++ SetMouse/main.cpp
@@ -1,8 +1,10 @@
 ﻿#include <Windows.h>
 #undef min
 #undef max
+#include <string>
 #include <functional>
 #include <chrono>
+#include <optional>
 
 #include <utils/window.hpp>
 #include <utils/timer_thread.hpp>
@@ -23,6 +25,7 @@ class main_window final : public window
 	}
 	BOOL OnCreate(HWND, LPCREATESTRUCT)
 	{
+		caption(L"SetMouse");
 		return TRUE;
 	}
 	void OnDestroy(HWND)
@@ -38,28 +41,66 @@ private:
 			throw std::runtime_error("fail to SystemParametersInfoW.");
 	}
 public:
+	/// <summary>
+	/// 判断是否开启鼠标加速。
+	/// </summary>
+	/// <returns>一个布尔值，表示开启鼠标加速。</returns>
 	static bool query_mouse_acceleration()
 	{
 		int params[3];
 		spi_interface(SPI_GETMOUSE, params);
 		return params[2];
 	}
+	/// <summary>
+	/// 开启鼠标加速。
+	/// </summary>
 	static void enable_mouse_acceleration()
 	{
 		constexpr int params[3]{ 6, 10, 1 };
 		spi_interface(SPI_SETMOUSE, params);
 	}
+	/// <summary>
+	/// 关闭鼠标加速。
+	/// </summary>
 	static void disable_mouse_acceleration()
 	{
 		constexpr int params[3]{};
 		spi_interface(SPI_SETMOUSE, params);
 	}
 
+public:
+	/// <summary>
+	/// 取焦点窗口句柄。
+	/// </summary>
+	/// <returns>焦点窗口句柄。</returns>
+	static HWND MyGetFocus()
+	{
+		HWND hwnd;
+		DWORD dwThreadId;
+		dwThreadId = GetWindowThreadProcessId(GetForegroundWindow(), 0);
+		AttachThreadInput(GetCurrentThreadId(), dwThreadId, TRUE);
+		hwnd = GetFocus();
+		AttachThreadInput(GetCurrentThreadId(), dwThreadId, FALSE);
+		return hwnd;
+	}
+	static std::wstring MyGetWindowText(HWND hwnd)
+	{
+		std::wstring ret(GetWindowTextLengthW(hwnd), L'\0');
+		if (ret.length())
+			GetWindowTextW(hwnd, ret.data(), ret.length() + 1);
+		return ret;
+	}
+
 private:
 	timer_thread tt{ std::bind(&main_window::bg_routine, this), 500 };
 	void bg_routine()
 	{
-
+		using namespace std::string_literals;
+		auto matched_window_text = L"Memory Cleaner"s;
+		if (MyGetWindowText(MyGetFocus()) == matched_window_text)
+			disable_mouse_acceleration();
+		else
+			enable_mouse_acceleration();
 	}
 };
 
